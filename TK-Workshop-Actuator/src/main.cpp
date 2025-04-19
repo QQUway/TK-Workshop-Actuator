4a4dfe0d7f4079e45b784789cc78813eeae2a2cb
NatzNatz <125758574+QQUway@users.noreply.github.com>
2025-04-19 16:27:03 +0700

    Iterasi 1
    - BLE Advertise

--- TK-Workshop-Actuator/src/main.cpp
+++ TK-Workshop-Actuator/src/main.cpp
@@ -1,18 +1,70 @@
 #include <Arduino.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define SERVICE_UUID        "4869e6e5-dec6-4a9d-a0a4-eda6b5448b97"
+#define CHARACTERISTIC_UUID "05c4d03a-ac78-4627-8778-f23fab166ba8"
+
+#define BUZZER_PIN 27 // GPIO pin for the buzzer
+
+
+class MyServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    Serial.println("Client connected");
+  }
+
+  void onDisconnect(BLEServer* pServer) {
+    Serial.println("Client disconnected");
+  }
+};
+
+class MyCallbacks : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *pCharacteristic) {
+    std::string value = pCharacteristic->getValue();
+
+    Serial.print("Received Value: ");
+    Serial.println(value.c_str());
+
+    if (value.length() > 0) {
+      Serial.print("Received Value: ");
+      Serial.println(value.c_str());
+
+      if (value == "1") {
+        digitalWrite(BUZZER_PIN, HIGH);
+        delay(500); // buzz for 500ms
+        digitalWrite(BUZZER_PIN, LOW);
+      } else {
+        digitalWrite(BUZZER_PIN, LOW);
+      }
+    }
+  }
+};
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+
+  BLEDevice::init("ESP32_WS_Output");
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLECharacteristic *pCharacteristic =
+    pService->createCharacteristic(CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+  pCharacteristic->setCallbacks(new MyCallbacks());
+  pService->start();
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(2000);
 }
